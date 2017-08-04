3cdc84a44141f4502fc1c30bb00eaa21552dddae
David <David@DAVID-PC.JG.Computers.Grant>
2017-08-04 14:10:05 +0100

    Can now check if password have letters and numbers and upper and lower case.

--- src/io/github/davidg95/JTill/jtill/Validator.java
+++ src/io/github/davidg95/JTill/jtill/Validator.java
@@ -11,6 +11,9 @@ package io.github.davidg95.JTill.jtill;
  */
 public class Validator {
 
+    public static boolean UPPER_AND_LOWER = false;
+    public static boolean LETTERS_AND_NUMBERS = false;
+
     /**
      * Checks that an email contains a . and a @. Also checks that the length is
      * greater than 4.
@@ -44,6 +47,36 @@ public class Validator {
         }
         if (password.length() < 6) {
             return false;
+        }
+        if (UPPER_AND_LOWER) {
+            boolean hasUpper = false;
+            boolean hasLower = false;
+            for (char c : password.toCharArray()) {
+                if (Character.isUpperCase(c)) {
+                    hasUpper = true;
+                }
+                if (Character.isLowerCase(c)) {
+                    hasLower = true;
+                }
+            }
+            if (!hasUpper || !hasLower) {
+                return false;
+            }
+        }
+        if (LETTERS_AND_NUMBERS) {
+            boolean hasLetter = false;
+            boolean hasNumber = false;
+            for (char c : password.toCharArray()) {
+                if (Character.isLetter(c)) {
+                    hasLetter = true;
+                }
+                if (Character.isDigit(c)) {
+                    hasNumber = true;
+                }
+            }
+            if (!hasLetter || !hasNumber) {
+                return false;
+            }
         }
         return true;
     }
