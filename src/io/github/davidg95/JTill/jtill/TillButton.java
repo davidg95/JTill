b544bea0d6498fd363bfe5cf0b74b7cc69f6f0ac
David <David@DAVID-PC.JG.Computers.Grant>
2017-10-02 13:20:28 +0100

    Added logoff function to custom buttons

--- src/io/github/davidg95/JTill/jtill/TillButton.java
+++ src/io/github/davidg95/JTill/jtill/TillButton.java
@@ -100,6 +100,10 @@ public class TillButton implements Serializable {
      * Indicates the button is to go to the tills main screen.
      */
     public static final int MAIN = 5;
+    /**
+     * Indicates that the button is to log off.
+     */
+    public static final int LOGOFF = 6;
 
     public TillButton(String name, int item, int type, int screen, int color, int width, int height, int x, int y) {
         this.name = name;
