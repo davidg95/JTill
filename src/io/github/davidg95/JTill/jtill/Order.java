889bd46b9432bab90fd45f0c8d3d15ce31f328dd
David <David@DAVID-PC.JG.Computers.Grant>
2017-11-07 13:50:12 +0000

    Can now receive orders

--- src/io/github/davidg95/JTill/jtill/Order.java
+++ src/io/github/davidg95/JTill/jtill/Order.java
@@ -20,10 +20,12 @@ public class Order {
     private boolean sent;
     private Date sendDate;
     private List<OrderItem> items;
+    private boolean received;
 
-    public Order(int id, Supplier supplier, boolean sent, Date sendDate, List<OrderItem> items) {
+    public Order(int id, Supplier supplier, boolean sent, Date sendDate, List<OrderItem> items, boolean received) {
         this(supplier, sent, sendDate, items);
         this.id = id;
+        this.received = received;
     }
 
     public Order(Supplier supplier, boolean sent, Date sendDate, List<OrderItem> items) {
@@ -31,6 +33,7 @@ public class Order {
         this.sent = sent;
         this.sendDate = sendDate;
         this.items = items;
+        received = false;
     }
 
     public Order(Supplier s, List<OrderItem> items) {
@@ -38,6 +41,7 @@ public class Order {
         this.items = items;
         sent = false;
         this.sendDate = new Date(0);
+        received = false;
     }
 
     public int getId() {
@@ -72,6 +76,13 @@ public class Order {
         this.sendDate = sendDate;
     }
 
+    public boolean isReceived() {
+        return received;
+    }
+
+    public void setReceived(boolean received) {
+        this.received = received;
+    }
     public List<OrderItem> getItems() {
         return items;
     }
