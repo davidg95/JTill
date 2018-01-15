d0e175f96d7f4bacb29312f1f4e78ec2a2402622
David <David@DAVID-PC.JG.Computers.Grant>
2018-01-15 11:20:52 +0000

    Added progress bar to ModalDialog

--- src/io/github/davidg95/JTill/jtill/Department.java
+++ src/io/github/davidg95/JTill/jtill/Department.java
@@ -10,8 +10,6 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
